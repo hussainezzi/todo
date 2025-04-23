931e05d4302f58c3e013d5877d7fcf5b6ee3e58c
hussainezzi <hussainezzi03@gmail.com>
2025-04-23 16:21:57 +0500

    Add ListTodo component with initial state setup and layout

--- src/app/ListTodo/page.tsx
+++ src/app/ListTodo/page.tsx
@@ -1,3 +1,23 @@
 import {useState} from 'react';
+import {useRouter} from 'next/navigation';
+import {Todo} from '@prisma/client';
+import {useEffect } from 'react';
 
-impo
+export default function ListTodo(){
+
+    const [todos , setTodos]  = useState<Todo[]>([])
+    const router = useRouter();
+    
+    
+
+    return(
+        <>
+         <div className='flex flex-col justify-center items-center bg-grey-500'>
+            <div classNa-me='text-3xl text-white bg-blue-800'> List Todo</>
+
+            
+            </div>
+        
+        </>
+    )
+}
