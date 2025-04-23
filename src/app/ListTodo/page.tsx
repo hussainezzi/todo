f71241d6b0f80976606b6e703b8247b509fc3f3f
hussainezzi <hussainezzi03@gmail.com>
2025-04-23 16:27:27 +0500

    Enhance ListTodo component layout and display todos with improved styling

--- src/app/ListTodo/page.tsx
+++ src/app/ListTodo/page.tsx
@@ -7,13 +7,21 @@ export default function ListTodo(){
 
     const [todos , setTodos]  = useState<Todo[]>([])
     const router = useRouter();
-    
+
     
 
     return(
         <>
-         <div className='flex flex-col justify-center items-center bg-grey-500'>
-            <div classNa-me='text-3xl text-white bg-blue-800'> List Todo</>
+         <div className='flex flex-col gap-4 justify-center items-center h-screen bg-gray-900'>
+            <div className= 'text-3xl text-white bg-blue-800'  > List Todo</div>
+            <div className='flex flex-col gap-4 '>
+                {todos.map((todo)=>(
+                    <div key={todo.id} className='bg-gray-800 text-white p-4 rounded-md shadow-md'>
+                        <h2 className='text-xl font-bold'>{todo.title}</h2>
+                        <p className='text-gray-400'>ID: {todo.id}</p>
+                    </div>
+                ))}
+                </div>
 
             
             </div>
